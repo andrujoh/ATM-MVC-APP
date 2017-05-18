125b2ec01ad835913c14d364faf9bb8c2b64ea98
andrujoh <andru_joh@yahoo.no>
2017-05-18 10:09:34 +0200

    Add project files.

---
+++ ATM-app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATM_app.Startup))]
+namespace ATM_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
